dfd5d7b63906dc994146261a8d6d057fb44d40a0
AdamC33 <ac1904@student.aru.ac.uk>
2022-11-16 09:41:15 +0000

    Extension task

--- Program.cs
+++ Program.cs
@@ -1,9 +1,41 @@
-﻿float[] floatArray = new float[5];
-floatArray[0] = 3.4F;
-floatArray[1] = 6.7F;
-floatArray[2] = 8.3F;
-floatArray[3] = 9.0F;
-floatArray[4] = 2.4F;
+﻿int arrayLength = 0;
+do
+{
+    Console.WriteLine("How many numbers do you want to enter?");
+    try
+    {
+        arrayLength = Convert.ToInt32(Console.ReadLine());
+    }
+    catch (System.FormatException) //If the user doesn't input an integer value, it will show them this error, then ask them again for the array length.
+    {
+        arrayLength = 0;
+        Console.WriteLine("Please only enter integer values.");
+    }
+}
+while (arrayLength < 1);
+
+float[] floatArray = new float[arrayLength];
+for (int i = 0; i < floatArray.Length; i++)
+{
+    bool floatParseSuccessful = true;
+    do
+    {
+        floatParseSuccessful = true; //Every time the loop starts this is true. It only goes to false if an exception is caught.
+        Console.WriteLine("Enter a value for float "+(i+1));
+        try
+        {
+            floatArray[i] = Single.Parse(Console.ReadLine()); //Created by adapting examples shown at
+            //https://learn.microsoft.com/en-us/dotnet/api/system.single.parse?view=net-6.0
+        }
+        catch (System.FormatException)
+        {
+            floatParseSuccessful = false;
+            floatArray[i] = 0;
+            Console.WriteLine("Please only enter float values.");
+        }
+    }
+    while (floatParseSuccessful == false);
+}
 
 float result = 0.0F;
 
