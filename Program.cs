3be823f3d857235ed88d72f341f0d97bdf01e11f
AdamC33 <ac1904@student.aru.ac.uk>
2022-11-16 09:11:29 +0000

    Outputs mean of an array

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿float[] floatArray = new float[5];
+floatArray[0] = 3.4F;
+floatArray[1] = 6.7F;
+floatArray[2] = 8.3F;
+floatArray[3] = 9.0F;
+floatArray[4] = 2.4F;
+
+float result = 0.0F;
+
+for (int i = 0; i < floatArray.Length; i++)
+{
+    result += floatArray[i];
+}
+
+Console.WriteLine(result / floatArray.Length);
+
+
+Console.ReadKey();
